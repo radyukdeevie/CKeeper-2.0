42b8c296d82fd50d7b1595f0175b6789a99328ee
Denis <deevie.official@gmail.com>
2024-09-17 00:44:10 +0300

    end

---
+++ _bar.py
@@ -0,0 +1,160 @@
+import sys
+import psycopg2 as pg
+from PyQt5 import QtWidgets, QtCore
+from functools import partial
+from _contructors.bar_build import create_ui, set_btn, set_font
+
+class UI_MainWindow(object):
+    def __init__(self, conn):
+        self.conn = conn
+        self.cur = conn.cursor()
+    
+    def setup_ui(self, MainWindow):
+        elements = create_ui(MainWindow)
+        self.centralwidget = elements["centralwidget"]
+        self.name_label = elements["name_label"]
+        self.gridLayout = elements["gridLayout"]
+        self.tabWidget = elements["tabWidget"]
+        self.active_order = elements["active_order"]
+        self.gridLayout_2 = elements["gridLayout_2"]
+        self.scrollArea = elements["scrollArea"]
+        self.scrollArea_2 = elements["scrollArea_2"]
+        self.close_order = elements["close_order"]
+        self.gridLayout_3 = elements["gridLayout_3"]
+        self.scrollAreaWidgetContents = elements["scrollAreaWidgetContents"]
+
+        
+        self.order_container = QtWidgets.QWidget()
+        self.order_layout = QtWidgets.QVBoxLayout(self.order_container)
+        self.order_container_2 = QtWidgets.QWidget()
+        self.order_layout_2 = QtWidgets.QVBoxLayout(self.order_container_2)
+        self.scrollArea.setWidget(self.order_container)
+        self.scrollArea_2.setWidget(self.order_container_2)
+        self.set_active_order()
+        self.set_close_order()
+    
+    def on_btn_clicked(self, btn_id):
+        print(f'Clicked {btn_id} button')
+        try:
+            self.cur.execute(
+                "UPDATE list_order SET product_status_id = 2 WHERE product_id = %s", (btn_id,)
+            )
+            self.conn.commit()
+            print(f'Updated product_id {btn_id} successfully')
+        except Exception as e:
+            print(f"Error occurred: {e}")
+        self.set_active_order()
+
+    def set_active_order(self):
+        self.cur.execute(
+            "SELECT product_name, lo.product_quantity, created_at, product_id "
+            "FROM list_order lo "
+            "JOIN product USING (product_id) "
+            "JOIN active_order USING (order_id) "
+            "WHERE product_status_id = 1 AND product.type_product_id = 2 "
+        )
+        rows = self.cur.fetchall()
+        product_name = [row[0] for row in rows]
+        product_quantity = [row[1] for row in rows]
+        create_at = [f'{row[2].hour:02}:{row[2].minute:02}' for row in rows]
+        product_id = [row[3] for row in rows]
+
+       
+        for i in reversed(range(self.order_layout.count())):
+            widget_to_remove = self.order_layout.itemAt(i).widget()
+            if widget_to_remove is not None:
+                widget_to_remove.deleteLater()
+
+        
+        for i in range(len(product_name)):
+            widget = QtWidgets.QWidget()
+            widget.setObjectName(f"order_widg_{i + 1}")
+            form_layout = QtWidgets.QFormLayout(widget)
+            form_layout.setObjectName(f"formLayout_{i + 1}")
+
+            
+            order_label = QtWidgets.QLabel(widget)
+            order_label.setFont(set_font("Century Gothic", 15, False))
+            order_label.setText(f"{product_name[i]} | {create_at[i]} | {product_quantity[i]}")
+            order_label.setObjectName(f"order_lbl_{i + 1}")
+            form_layout.setWidget(0, QtWidgets.QFormLayout.LabelRole, order_label)
+
+            
+            push_button = QtWidgets.QPushButton(widget)
+            push_button.setFont(set_font("Century Gothic", 12, False))
+            push_button.setText("Готов")
+            push_button.setFixedSize(200, 40)
+            set_btn(push_button)
+            push_button.setObjectName(f"order_btn_{i + 1}")
+            push_button.clicked.connect(partial(self.on_btn_clicked, product_id[i]))
+            form_layout.setWidget(0, QtWidgets.QFormLayout.FieldRole, push_button)
+            form_layout.setAlignment(push_button, QtCore.Qt.AlignRight)
+
+            
+            self.order_layout.addWidget(widget)
+
+        self.order_container.adjustSize()
+        self.scrollArea.update()
+    def set_close_order(self):
+        self.cur.execute(
+            "SELECT order_list_id, product_name, h_lo.product_quantity, h_lo.product_desc "
+            "FROM history_list_order h_lo "
+            "JOIN product USING (product_id) "
+            "WHERE product.type_product_id = 2"
+        )
+        rows = self.cur.fetchall()
+        order_list_id = [row[0] for row in rows]
+        product_name = [row[1] for row in rows]
+        product_quantity = [row[2] for row in rows]
+        product_desc = [row[3] for row in rows]
+        
+        print(order_list_id)
+        print(product_name)
+        print(product_quantity)
+        print(product_desc)
+        
+
+        for i in reversed(range(self.order_layout_2.count())):
+            widget_to_remove = self.order_layout_2.itemAt(i).widget()
+            if widget_to_remove is not None:
+                widget_to_remove.deleteLater()
+
+        for i in range(len(product_name)):
+            widget = QtWidgets.QWidget()
+            widget.setObjectName(f"history_order_widg_{i + 1}")
+            form_layout = QtWidgets.QFormLayout(widget)
+            form_layout.setObjectName(f"formLayout_{i + 1}")
+
+            order_label = QtWidgets.QLabel(widget)
+            order_label.setFont(set_font("Century Gothic", 15, False))
+            order_label.setText(f"{order_list_id[i]} | {product_name[i]} | {product_quantity[i]} | {product_desc[i]}")
+            order_label.setObjectName(f"history_order_lbl_{i + 1}")
+            form_layout.setWidget(0, QtWidgets.QFormLayout.LabelRole, order_label)
+
+            self.order_layout_2.addWidget(widget)
+
+        self.order_container_2.adjustSize()
+        self.scrollArea_2.update()
+
+
+def main():
+    conn = pg.connect(
+        host="localhost",
+        port="5432",
+        dbname="CKeeper",
+        user="postgres",
+        password="raiderfeed08032005"
+    )
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    MainWindow.resize(900, 600)
+    ui = UI_MainWindow(conn)
+    ui.setup_ui(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+    
+    conn.close()
+
+if __name__ == "__main__":
+    main()
---
+++ _contructors/bar_build.py
@@ -0,0 +1,97 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+# func for set settings for button
+def set_font(font_family, size, is_bold):
+    font = QtGui.QFont(font_family, size)
+    font.setBold(is_bold)
+    return font
+
+# func for default settings for buttons
+def set_btn(btn):
+    btn.setAutoFillBackground(False)
+    btn.setAutoDefault(False)
+    btn.setDefault(False)
+    btn.setFlat(False)
+
+def create_ui(MainWindow):
+    MainWindow.setWindowTitle("Бар")
+    
+    centralwidget = QtWidgets.QWidget(MainWindow)
+    centralwidget.setObjectName("centralwidget")
+    gridLayout = QtWidgets.QGridLayout(centralwidget)
+    gridLayout.setObjectName("gridLayout")
+
+    # name
+    name_label = QtWidgets.QLabel(centralwidget)
+    name_label.setFont(set_font("Century Gothic", 16, True))
+    name_label.setText("Бар")
+    name_label.setObjectName("name_label")
+    gridLayout.addWidget(name_label, 0, 0, 1, 1)
+
+    # tab widget for active and close orders
+    tabWidget = QtWidgets.QTabWidget(centralwidget)
+    tabWidget.setObjectName("tabWidget")
+
+    # active order tab
+    active_order = QtWidgets.QWidget()
+    active_order.setObjectName("active_order")
+    gridLayout_2 = QtWidgets.QGridLayout(active_order)
+    gridLayout_2.setObjectName("gridLayout_2")
+
+    scrollArea = QtWidgets.QScrollArea(active_order)
+    scrollArea.setWidgetResizable(True)
+    scrollArea.setObjectName("scrollArea")
+
+    # scrollArea.setWidget(order_widg_1)
+    gridLayout_2.addWidget(scrollArea, 0, 0, 1, 1)
+    tabWidget.addTab(active_order, "Активные")
+
+    # close order tab
+    close_order = QtWidgets.QWidget()
+    close_order.setObjectName("close_order")
+    gridLayout_3 = QtWidgets.QGridLayout(close_order)
+    gridLayout_3.setObjectName("gridLayout_3")
+
+    scrollArea_2 = QtWidgets.QScrollArea(close_order)
+    scrollArea_2.setWidgetResizable(True)
+    scrollArea_2.setObjectName("scrollArea")
+    scrollAreaWidgetContents = QtWidgets.QWidget()
+    scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 771, 361))
+    scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+
+    formLayout_2 = QtWidgets.QFormLayout(scrollAreaWidgetContents)
+    formLayout_2.setObjectName("formLayout_2")
+
+    close_lbl_1 = QtWidgets.QLabel(scrollAreaWidgetContents)
+    close_lbl_1.setFont(set_font("Century Gothic", 15, False))
+    close_lbl_1.setText("Круассан с лососем | 14:48 | 15:35")
+    close_lbl_1.setObjectName("close_lbl_1")
+    formLayout_2.setWidget(0, QtWidgets.QFormLayout.LabelRole, close_lbl_1)
+
+    close_btn_1 = QtWidgets.QPushButton(scrollAreaWidgetContents)
+    close_btn_1.setFont(set_font("Century Gothic", 12, False))
+    close_btn_1.setText("Готов")
+    formLayout_2.setWidget(0, QtWidgets.QFormLayout.FieldRole, close_btn_1)
+
+    scrollArea_2.setWidget(scrollAreaWidgetContents)
+    gridLayout_3.addWidget(scrollArea_2, 0, 0, 1, 1)
+    tabWidget.addTab(close_order, "Закрытые")
+
+    gridLayout.addWidget(tabWidget, 1, 0, 1, 1)
+    MainWindow.setCentralWidget(centralwidget)
+
+    QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    return {
+        "centralwidget": centralwidget,
+        "name_label": name_label,
+        "gridLayout": gridLayout,
+        "tabWidget": tabWidget,
+        "active_order": active_order,
+        "gridLayout_2": gridLayout_2,
+        "scrollArea": scrollArea,
+        "scrollArea_2": scrollArea_2,
+        "close_order": close_order,
+        "gridLayout_3": gridLayout_3,
+        "scrollAreaWidgetContents": scrollAreaWidgetContents
+    }
