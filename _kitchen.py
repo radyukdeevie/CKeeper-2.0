7c8223032a5831a70396a09cbfa48b849d63178c
Denis <deevie.official@gmail.com>
2024-09-17 00:43:37 +0300

    end monitors: - Monitor Kitchen - Monitor Bar

--- _kitchen.py
+++ _kitchen.py
@@ -23,12 +23,14 @@ class UI_MainWindow(object):
         self.gridLayout_3 = elements["gridLayout_3"]
         self.scrollAreaWidgetContents = elements["scrollAreaWidgetContents"]
 
-        # Контейнер для виджетов заказов внутри scrollArea
         self.order_container = QtWidgets.QWidget()
         self.order_layout = QtWidgets.QVBoxLayout(self.order_container)
+        self.order_container_2 = QtWidgets.QWidget()
+        self.order_layout_2 = QtWidgets.QVBoxLayout(self.order_container_2)
         self.scrollArea.setWidget(self.order_container)
-  
+        self.scrollArea_2.setWidget(self.order_container_2)
         self.set_active_order()
+        self.set_close_order()
     
     def on_btn_clicked(self, btn_id):
         print(f'Clicked {btn_id} button')
@@ -48,36 +50,31 @@ class UI_MainWindow(object):
             "FROM list_order lo "
             "JOIN product USING (product_id) "
             "JOIN active_order USING (order_id) "
-            "WHERE product_status_id = 1"
+            "WHERE product_status_id = 1 AND product.type_product_id = 1 "
         )
         rows = self.cur.fetchall()
         product_name = [row[0] for row in rows]
         product_quantity = [row[1] for row in rows]
         create_at = [f'{row[2].hour:02}:{row[2].minute:02}' for row in rows]
         product_id = [row[3] for row in rows]
-        print(product_id)
 
-        # Очистить старые виджеты из контейнера
         for i in reversed(range(self.order_layout.count())):
             widget_to_remove = self.order_layout.itemAt(i).widget()
             if widget_to_remove is not None:
                 widget_to_remove.deleteLater()
 
-        # Добавляем новые виджеты заказов
         for i in range(len(product_name)):
             widget = QtWidgets.QWidget()
             widget.setObjectName(f"order_widg_{i + 1}")
             form_layout = QtWidgets.QFormLayout(widget)
             form_layout.setObjectName(f"formLayout_{i + 1}")
 
-            # Создание метки с информацией о заказе
             order_label = QtWidgets.QLabel(widget)
             order_label.setFont(set_font("Century Gothic", 15, False))
             order_label.setText(f"{product_name[i]} | {create_at[i]} | {product_quantity[i]}")
             order_label.setObjectName(f"order_lbl_{i + 1}")
             form_layout.setWidget(0, QtWidgets.QFormLayout.LabelRole, order_label)
 
-            # Создание кнопки "Готов"
             push_button = QtWidgets.QPushButton(widget)
             push_button.setFont(set_font("Century Gothic", 12, False))
             push_button.setText("Готов")
@@ -88,12 +85,51 @@ class UI_MainWindow(object):
             form_layout.setWidget(0, QtWidgets.QFormLayout.FieldRole, push_button)
             form_layout.setAlignment(push_button, QtCore.Qt.AlignRight)
 
-            # Добавляем созданный виджет в контейнер
             self.order_layout.addWidget(widget)
 
-        # Обновление области прокрутки
         self.order_container.adjustSize()
         self.scrollArea.update()
+    def set_close_order(self):
+        self.cur.execute(
+            "SELECT order_list_id, product_name, h_lo.product_quantity, h_lo.product_desc "
+            "FROM history_list_order h_lo "
+            "JOIN product USING (product_id) "
+            "WHERE product.type_product_id = 1 "
+        )
+        rows = self.cur.fetchall()
+        order_list_id = [row[0] for row in rows]
+        product_name = [row[1] for row in rows]
+        product_quantity = [row[2] for row in rows]
+        product_desc = [row[3] for row in rows]
+        
+        print(order_list_id)
+        print(product_name)
+        print(product_quantity)
+        print(product_desc)
+        
+        for i in reversed(range(self.order_layout_2.count())):
+            widget_to_remove = self.order_layout_2.itemAt(i).widget()
+            if widget_to_remove is not None:
+                widget_to_remove.deleteLater()
+
+        for i in range(len(product_name)):
+            widget = QtWidgets.QWidget()
+            widget.setObjectName(f"history_order_widg_{i + 1}")
+            form_layout = QtWidgets.QFormLayout(widget)
+            form_layout.setObjectName(f"formLayout_{i + 1}")
+
+            order_label = QtWidgets.QLabel(widget)
+            order_label.setFont(set_font("Century Gothic", 15, False))
+            order_label.setText(f"{order_list_id[i]} | {product_name[i]} | {product_quantity[i]} | {product_desc[i]}")
+            order_label.setObjectName(f"history_order_lbl_{i + 1}")
+            form_layout.setWidget(0, QtWidgets.QFormLayout.LabelRole, order_label)
+
+
+            self.order_layout_2.addWidget(widget)
+
+        self.order_container_2.adjustSize()
+        self.scrollArea_2.update()
+
 
 def main():
     conn = pg.connect(
@@ -112,7 +148,6 @@ def main():
     MainWindow.show()
     sys.exit(app.exec_())
     
-    # Закрываем соединение после завершения работы приложения
     conn.close()
 
 if __name__ == "__main__":
